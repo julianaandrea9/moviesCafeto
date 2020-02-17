0847ec7a4919ba6236965b236bf10b1dfe2aec4b
juliana andrea <julianaandreabuitrago@gmail.com>
2020-02-16 22:58:06 -0500

    Se hace consumo de api auth0, se crea vista de logueo y usuario

--- server.js
+++ server.js
@@ -7,12 +7,26 @@ const morgan = require('morgan')
 const config = require('./config/application')
 const routes = require('./routes/routes')
 const bodyParser = require('body-parser')
-const app = express()
+
+/**
+ * Required External Modules
+ */
+
+const path = require("path");
+
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+let app = express()
+
+require("dotenv").config();
+
+const authRouter = require("./auth");
 
 mongoose.Promise = global.Promise
 
 mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-  let app = express()
+  
   const securePort = process.env.PORT || '8087'
   console.log('Mongo is connected ' + securePort)
 
@@ -36,3 +50,95 @@ mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: t
     console.log('Server started at http://localhost:8087');
   })
 }).catch(err => console.log(err))
+
+/**
+ * Session Configuration
+ */
+const session = {
+  secret: "LoxodontaElephasMammuthusPalaeoloxodonPrimelephas",
+  cookie: {},
+  resave: false,
+  saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+}
+
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:8087/callback"
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
+
+ /**
+ *  App Configuration
+ */
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// Creating custom middleware with Express
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
+/**
+ * Routes Definitions
+ */
+
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+};
+
+app.get("/");
+
+app.get("/user", secured, (req, res, next) => {
+  const { _raw, _json, ...userProfile } = req.user;
+  res.render("user", {
+    title: "Profile",
+    userProfile: userProfile
+  });
+});
+
+// Router mounting
+app.use("/", authRouter);
